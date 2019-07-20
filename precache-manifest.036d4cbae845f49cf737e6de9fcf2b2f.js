106f4aeec56a034a9fa171bd43ce2b717a9b3697
Mike Huitrado <huitrado.mike@gmail.com>
2019-07-19 21:21:53 -0300

    Updates

---
+++ precache-manifest.036d4cbae845f49cf737e6de9fcf2b2f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55c3c6dcda6003bf1df06b6cf936a15",
+    "url": "/github-repos-directory-reactjs/index.html"
+  },
+  {
+    "revision": "5b0a85549240152f76d5",
+    "url": "/github-repos-directory-reactjs/static/js/2.e6b06eea.chunk.js"
+  },
+  {
+    "revision": "6a0afe8903d528cb4524",
+    "url": "/github-repos-directory-reactjs/static/js/main.c92438cc.chunk.js"
+  },
+  {
+    "revision": "0d4f64bf457fbe43232d",
+    "url": "/github-repos-directory-reactjs/static/js/runtime~main.24288584.js"
+  }
+]);
